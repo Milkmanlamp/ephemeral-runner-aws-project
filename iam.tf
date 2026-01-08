97b9d0bc49176fbc181fbb16450d5b7f774fe95f
Milkmanlamp <dany.phillips@pm.me>
2026-01-08 12:27:23 +1100

    setting up iam policys

--- iam.tf
+++ iam.tf
@@ -1,1 +1,23 @@
+resource "aws_iam_role" "ecs_exec" {
+  name = "${var.proj_name}-ecs-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
 
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.proj_name}-lambda-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
