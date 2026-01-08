d29f0a095a2b52d6afec0db9a95ac6e774173dac
Milkmanlamp <dany.phillips@pm.me>
2026-01-08 12:28:57 +1100

    setting up iam policys

--- iam.tf
+++ iam.tf
@@ -21,3 +21,40 @@ resource "aws_iam_role" "lambda_exec" {
     }]
   })
 }
+resource "aws_iam_role_policy" "ecs_ssm" {
+  name = "ssm-access"
+  role = aws_iam_role.ecs_exec.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect   = "Allow"
+      Action   = ["ssm:GetParameters", "ssm:GetParameter"]
+      Resource = "arn:aws:ssm:ap-southeast-2:*:parameter/github-runner/*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "lambda_perms" {
+  name = "lambda-perms"
+  role = aws_iam_role.lambda_exec.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = ["ecs:RunTask"]
+        Resource = "*" # TODO: lock down to task def arn later
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["iam:PassRole"]
+        Resource = aws_iam_role.ecs_exec.arn
+      },
+      {
+        Effect   = "Allow"
+        Action   = ["ssm:GetParameter", "ssm:GetParameters"]
+        Resource = "arn:aws:ssm:ap-southeast-2:*:parameter/github-runner/*"
+      }
+    ]
+  })
+}
